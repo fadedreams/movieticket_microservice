5e39eecacc25d26bf8a0aebd7cbe8d486d617f76
fadedreams7 <fadedreams7@gmail.com>
2023-11-09 10:17:26 -0500

    some errH+

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
-
+//import { RequestValidationError } from "../errors/request-validation-error";
+//import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { CustomError } from '../errors/custom-error';
 interface FormattedError {
   message: string;
   field?: string;
@@ -13,17 +13,17 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof RequestValidationError) {
+  if (err instanceof CustomError) {
     //const formattedErrors: FormattedError[] = err.errors.map((error) => {
     //return { message: error?.msg, field: error?.type } as FormattedError;
     //});
     //return res.status(400).send({ errors: formattedErrors });
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
-  if (err instanceof DatabaseConnectionError) {
-    return res.status(err.statusCode).send({ errors: [{ message: err.serializeErrors() }] });
-    //return res.status(500).send({ errors: [{ message: err.reason }] });
-  }
+  //if (err instanceof DatabaseConnectionError) {
+  //return res.status(err.statusCode).send({ errors: [{ message: err.serializeErrors() }] });
+  //return res.status(500).send({ errors: [{ message: err.reason }] });
+  //}
 
   res.status(400).send({
     errors: [{ message: "Something went wrong" }],
