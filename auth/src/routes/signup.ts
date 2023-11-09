5575936a0f0130084c7f72e0a221a716d126a66e
fadedreams7 <fadedreams7@gmail.com>
2023-11-09 08:17:50 -0500

    errHMid+

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -9,14 +9,14 @@ router.post(
     body("email").isEmail().withMessage("Email must be valid"),
     body("password")
       .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be between 4 and 20 characters"),
+      .isLength({ min: 1, max: 20 })
+      .withMessage("Password must be between 1 and 20 characters"),
   ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new Error('invalid Email or Password');
     }
     const { email, password } = req.body;
 
