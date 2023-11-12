06e55c9ea7798b11e4cec1d10a657366662a0d86
fadedreams7 <fadedreams7@gmail.com>
2023-11-12 00:04:31 -0500

    create ticket+

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,30 +1,37 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@fadedreams7org1/common';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@fadedreams7org1/common';
+import { Ticket } from '../models/ticket';
 import { currentUser } from '@fadedreams7org1/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', currentUser, requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
-
-export { router as createTicketRouter };
-
-////////////////////////
-//import express from 'express';
-//import { currentUser } from '../middlewares/current-user';
-//import { requireAuth } from '../middlewares/require-auth';
-
-//import { currentUser } from '@fadedreams7org1/common';
-//import { requireAuth } from '@fadedreams7org1/common';
-
-//const router = express.Router();
-
-//router.get('/api/users/currentuser2', currentUser, requireAuth, (req, res) => {
-////router.get('/api/users/currentuser', currentUser, (req, res) => {
-//console.log(req.session);
-//res.send({ currentUser: req.currentUser || null });
+//router.post('/api/tickets', currentUser, requireAuth, (req: Request, res: Response) => {
+//res.sendStatus(200);
 //});
 
-//export { router as currentUserRouter };
+router.post(
+  '/api/tickets', currentUser,
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
+  }
+);
+export { router as createTicketRouter };
 
